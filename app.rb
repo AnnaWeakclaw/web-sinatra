7d521c8916d426daff93169c3eff39695541a3f1
Anna <annas.wieclaw@gmail.com>
2019-05-29 13:43:34 +0100

    Add a form to process user input the send it to the action endpoint

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get '/testing' do
   "Testing route added"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @random_name = params[:name]
   erb(:index)
 end
